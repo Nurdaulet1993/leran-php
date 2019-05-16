b19c1b249ece4b9191c9ad808f62b3b93554d420
Nurdaulet <nurik93sat@gmail.com>
2019-05-17 00:47:30 +0600

    19 тый урок прошел

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 $str = 'Hello my der friend!';
 $mass= ['Satbergen', 'Nurdaulet'];
 
+/**Созлает массив из текста */
 $res = explode(' ', $str);
 $res2 = explode(' ', $str, 2);
 
+/**Обьединяет в массив текст */
 $res3 = implode(' ', $mass);
 $res4 = join(' ', $mass);
 
 print_r($res4);
+
+$str2 ="jhjhj\nllklkl\n";
+echo nl2br($str2);
+$str3 ='Нурдаулет';
+echo mb_strlen($str3);
